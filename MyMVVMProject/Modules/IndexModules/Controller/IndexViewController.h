aecbac44d9c4b36a6e28f0b0d55e8c428060a0bc
JingyuZhao <1563686808@qq.com>
2018-08-13 17:56:15 +0800

    no message

---
+++ MyMVVMProject/Modules/IndexModules/Controller/IndexViewController.h
@@ -0,0 +1,13 @@
+//
+//  IndexViewController.h
+//  MyMVVMProject
+//
+//  Created by winbei on 2018/8/13.
+//  Copyright © 2018年 HelloWorld_1986. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface IndexViewController : BaseViewController
+
+@end
---
+++ MyMVVMProject/Modules/IndexModules/Controller/IndexViewController.m
@@ -0,0 +1,13 @@
+//
+//  IndexViewController.m
+//  MyMVVMProject
+//
+//  Created by winbei on 2018/8/13.
+//  Copyright © 2018年 HelloWorld_1986. All rights reserved.
+//
+
+#import "IndexViewController.h"
+
+@implementation IndexViewController
+
+@end
